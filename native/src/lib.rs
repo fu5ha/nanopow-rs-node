baa28a825e0ad75228a223925692973b2d72a0f9
Gray Olson <gray@grayolson.com>
2018-02-21 13:01:03 -0700

    v0.3.0 - non blocking async support, improved documentation

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -4,15 +4,77 @@ extern crate neon;
 extern crate nanopow_rs;
 
 use neon::vm::{Call, JsResult};
-use neon::js::{JsString, JsBoolean, JsNumber};
+use neon::js::{JsString, JsBoolean, JsNumber, JsUndefined, JsFunction};
 use neon::js::error::JsError;
-use neon::js::error::Kind::SyntaxError;
+use neon::js::error::Kind;
+use neon::task::Task;
+use neon::scope::Scope;
 
-fn generate_work(call: Call) -> JsResult<JsString> {
+struct GenerateTask {
+    hash: String,
+    max_iters: Option<u64>,
+}
+
+impl GenerateTask {
+    pub fn new(hash: String, max_iters: Option<u64>) -> Self {
+        Self {
+            hash,
+            max_iters,
+        }
+    }
+}
+
+impl Task for GenerateTask {
+    type Output = String;
+    type Error = String;
+    type JsEvent = JsString;
+
+    fn perform(&self) -> Result<Self::Output, Self::Error> {
+        let result_work = nanopow_rs::generate_work(&self.hash, self.max_iters);
+        match result_work {
+            Some(work) => Ok(work),
+            None => Err(format!("Did not find valid work."))
+        }
+    }
+
+    fn complete<'a, T: Scope<'a>>(self, scope: &'a mut T, result: Result<Self::Output, Self::Error>) -> JsResult<Self::JsEvent> {
+        match result {
+            Ok(w) => Ok(JsString::new(scope, &w).unwrap()),
+            Err(e) => JsError::throw(Kind::Error, &e)
+        }
+    }
+}
+
+fn generate_work(call: Call) -> JsResult<JsUndefined> {
+    let scope = call.scope;
+    let args = call.arguments;
+    if args.len() != 3 {
+        return JsError::throw(Kind::SyntaxError, "Incorrect number of arguments.")
+    };
+
+    let in_hash = args.require(scope, 0)?.check::<JsString>()?.value();
+    
+    let max_iters = {
+        let iters = args.require(scope, 1)?.check::<JsNumber>()?.value() as u64;
+        if iters == 0 {
+            None
+        } else {
+            Some(iters)
+        }
+    };
+
+    let cb = args.require(scope, 2)?.check::<JsFunction>()?;
+
+    let gen_task = GenerateTask::new(in_hash, max_iters);
+    gen_task.schedule(cb);
+    Ok(JsUndefined::new())
+}
+
+fn generate_work_sync(call: Call) -> JsResult<JsString> {
     let scope = call.scope;
     let args = call.arguments;
     if args.len() != 2 {
-        return JsError::throw(SyntaxError, "Incorrect number of arguments.")
+        return JsError::throw(Kind::SyntaxError, "Incorrect number of arguments.")
     };
 
     let in_hash = args.require(scope, 0)?.check::<JsString>()?.value();
@@ -35,7 +97,7 @@ fn check_work(call: Call) -> JsResult<JsBoolean> {
     let scope = call.scope;
     let args = call.arguments;
     if args.len() != 2 {
-        return JsError::throw(SyntaxError, "Incorrect argument length.")
+        return JsError::throw(Kind::SyntaxError, "Incorrect argument length.")
     };
 
     let in_hash = args.require(scope, 0)?.check::<JsString>()?.value();
@@ -47,6 +109,7 @@ fn check_work(call: Call) -> JsResult<JsBoolean> {
 
 register_module!(m, {
     m.export("generateWork", generate_work)?;
+    m.export("generateWorkSync", generate_work_sync)?;
     m.export("checkWork", check_work)?;
     Ok(())
 });
